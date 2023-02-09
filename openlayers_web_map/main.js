c8c372917993f7d5b1a49850708bb90b62f65139
devin-lynch <devin.l.lynch@gmail.com>
2023-02-09 16:18:46 -0600

    Map controls

--- openlayers_web_map/main.js
+++ openlayers_web_map/main.js
@@ -8,22 +8,49 @@ window.onload = init // runs init function when window is fully loaded
 
 
 function init() {
+
+    // controls
+    const fullScreenControl = new ol.control.FullScreen() // fullscreen toggle top right
+    const mousePositionControl = new ol.control.MousePosition() // shows coordinates on mouse position top right
+    const overviewMapControl = new ol.control.OverviewMap({ // small inset map on bottom left
+        collapsed: false, // has the overview map open by default (true)
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM() // OSM = OpenStreetMap
+            })
+        ]
+    })
+    const scaleLineControl = new ol.control.ScaleLine()
+    const zoomSliderControl = new ol.control.ZoomSlider() // adds zoom slider on left screen
+    const zoomToExtentControl = new ol.control.ZoomToExtent() // adds full zoomout shortcut above slider
+
     const map = new ol.Map({
         view: new ol.View({
             center: [-12080385, 7567433],
             zoom: 3,
             maxZoom: 6, // zoom restrictions
             minZoom: 2, // zoom restrictions
-            rotation: 0.5 // rotates in clockwise direction. arrow in top right corner will reset rotation
+            rotation: 0,
+            // rotation: 0.5 // rotates in clockwise direction. arrow in top right corner will reset rotation
         }),
         layers: [
             new ol.layer.Tile({
-                source: new ol.source.OSM()
+                source: new ol.source.OSM() // OSM = OpenStreetMap
             })
         ],
         target: "js-map",
         keyboardEventTarget: document, // allows keyboard panning
+        controls: ol.control.defaults().extend([ // adding on to the default controls
+            fullScreenControl,
+            mousePositionControl,
+            overviewMapControl,
+            scaleLineControl,
+            zoomSliderControl,
+            zoomToExtentControl,
+        ])
     })
+
+    console.log(ol.control.defaults());
 
     const popupContainerElement = document.getElementById('popup-coordinates')
     const popup = new ol.Overlay({
