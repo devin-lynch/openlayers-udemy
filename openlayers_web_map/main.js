621aa21aaad46b733c8e61edf30803f7ab8d90fd
devin-lynch <devin.l.lynch@gmail.com>
2023-02-09 13:40:43 -0600

    View properties

--- openlayers_web_map/main.js
+++ openlayers_web_map/main.js
@@ -1,15 +1,18 @@
-import { Map, View } from 'ol/Map.js'
-import TileLayer from 'ol/layer/Tile.js'
-import OSM from 'ol/source/OSM.js'
+// import { Map, View } from 'ol/Map.js'
+// import TileLayer from 'ol/layer/Tile.js'
+// import OSM from 'ol/source/OSM.js'
 
 window.onload = init // runs init function when window is fully loaded
 
 
 function init() {
-    const map = new Map({
+    const map = new ol.Map({
         view: new ol.View({
-            center: [0, 0],
-            zoom: 2
+            center: [-12080385, 7567433],
+            zoom: 3,
+            maxZoom: 6, // zoom restrictions
+            minZoom: 2, // zoom restrictions
+            rotation: 0.5 // rotates in clockwise direction. arrow in top right corner will reset rotation
         }),
         layers: [
             new ol.layer.Tile({
